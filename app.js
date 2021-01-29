2636a4c2127c6e2ee1f5a848be52e50bb7f61c21
jmar76 <jmaguero.ing@gmail.com>
2021-01-29 05:44:22 +0000

    primera actualización

---
+++ app.js
@@ -0,0 +1,18 @@
+
+var pronoun  = [            "the","our","beautiful", "dog"              ];
+var adj      = [          "great", "big", "sonora", "lacust"            ];
+var noun     = [          "jogger","racon", "lunch", "cheap"            ];
+var dominium = [".com", ".es", ".net", ".us", ".edu", ".shop", ".online"];
+function imprimirMiDominio(){let str = a + c + e; return str;}
+                                                            let str = imprimirMiDominio();
+for (var i = 0; i < 7; i++)                        {
+    for (var n = 0; n <= pronoun.length; n++)      {
+        var a = (pronoun[Math.floor(Math.random()           *  pronoun.length   )]);}  
+    for (var n = 0; n <= adj.length; n++)          {
+        var c = (adj[Math.floor(Math.random()               *  adj.length       )]);}
+    for (var n = 0; n <= noun.length; n++)         {
+        var e = (noun[Math.floor(Math.random()              *  noun.length      )]);}
+    for (var n = 0; n <= dominium.length; n++)     {
+        var q = (dominium[Math.floor(Math.random()          *  dominium.length  )]);}  
+console.log(imprimirMiDominio() + q);}
+
