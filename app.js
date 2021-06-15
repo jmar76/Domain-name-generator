29dcd7960cd6fa5f9f9f2a1bbea37577a5c36923
jmar76 <jmaguero.ing@gmail.com>
2021-06-15 10:26:30 +0000

    corregido

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 
-var pronoun  = [            "the","our","beautiful", "dog"              ];
-var adj      = [          "great", "big", "sonora", "lacust"            ];
-var noun     = [          "jogger","racon", "lunch", "cheap"            ];
-var dominium = [".com", ".es", ".net", ".us", ".edu", ".shop", ".online"];
-function imprimirMiDominio(){let str = a + c + e; return str;}
-                                                            let str = imprimirMiDominio();
-for (var i = 0; i < 7; i++)                        {
-    for (var n = 0; n <= pronoun.length; n++)      {
-        var a = (pronoun[Math.floor(Math.random()           *  pronoun.length   )]);}  
-    for (var n = 0; n <= adj.length; n++)          {
-        var c = (adj[Math.floor(Math.random()               *  adj.length       )]);}
-    for (var n = 0; n <= noun.length; n++)         {
-        var e = (noun[Math.floor(Math.random()              *  noun.length      )]);}
-    for (var n = 0; n <= dominium.length; n++)     {
-        var q = (dominium[Math.floor(Math.random()          *  dominium.length  )]);}  
-console.log(imprimirMiDominio() + q);}
+let pronoun = ["the", "our", "beautiful", "dog"];
+let adj = ["great", "big", "sonora", "lacust"];
+let noun = ["jogger", "racon", "lunch", "cheap"];
+let dominium = [".com", ".es", ".net", ".us", ".edu", ".shop", ".online"];
 
+
+
+function imprimirMiDominio() {
+
+    for (let m = 0; m < pronoun.length; m++) {
+        for (let n = 0; n < adj.length; n++) {
+            for (let i = 0; i < noun.length; i++) {
+                for (let x = 0; x < dominium.length; x++) {
+                    let str = pronoun[m] + adj[n] + noun[i] + dominium[x];
+                    console.log(str);
+                }
+            }
+        }
+    }
+}
+imprimirMiDominio();
